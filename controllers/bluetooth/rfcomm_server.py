aa6ec357f931f7cd18d66cae34a3771ec81e7cfb
Meriem <meriem@crowtec.eu>
2017-05-25 13:14:55 +0200

    rfcomm server and motors

--- controllers/bluetooth/rfcomm_server.py
+++ controllers/bluetooth/rfcomm_server.py
@@ -1,9 +1,3 @@
-# file: rfcomm-server.py
-# auth: Albert Huang <albert@csail.mit.edu>
-# desc: simple demonstration of a server application that uses RFCOMM sockets
-#
-# $Id: rfcomm-server.py 518 2007-08-10 07:20:07Z albert $
-
 from bluetooth import *
 
 server_sock=BluetoothSocket( RFCOMM )
@@ -17,24 +11,26 @@ uuid = "94f39d29-7d6d-437d-973b-fba39e49d4ee"
 advertise_service( server_sock, "SampleServer",
                    service_id = uuid,
                    service_classes = [ uuid, SERIAL_PORT_CLASS ],
-                   profiles = [ SERIAL_PORT_PROFILE ], 
-#                   protocols = [ OBEX_UUID ] 
-                    )
-                   
+                   profiles = [ SERIAL_PORT_PROFILE ],
+                   #                   protocols = [ OBEX_UUID ]
+                   )
+
 print("Waiting for connection on RFCOMM channel %d" % port)
 
-client_sock, client_info = server_sock.accept()
-print("Accepted connection from ", client_info)
+while True:
 
-try:
-    while True:
-        data = client_sock.recv(1024)
-        if len(data) == 0: break
-        print("received [%s]" % data)
-except IOError:
-    pass
+    client_sock, client_info = server_sock.accept()
+    print("Accepted connection from ", client_info)
 
-print("disconnected")
+    try:
+        while True:
+            data = client_sock.recv(1024)
+            if len(data) == 0:
+                break
+            print("received [%s]" % data)
+    except IOError:
+        client_sock.close()
+        print("disconnected")
 
 client_sock.close()
 server_sock.close()
