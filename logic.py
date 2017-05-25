59a55e070b2782ea21556f059433038008b19fb8
Rodrigo Crespo <rcrespocepeda@gmail.com>
2017-05-25 13:45:11 +0200

    more logic

--- logic.py
+++ logic.py
@@ -1,72 +1,190 @@
-self.map = {
-    tiles: [
-        {
-            position: [0, 0],
-            status: DEAD_END,
-            begin: True,
-            end: False,
-            input_dir: [],
-            output_dirs: [],
-            possible_dirs: [NORTH]
-            forbidden_dirs: [WEST, SOUTH, EAST]
-        }
-    ]
+#!/usr/bin/python
+import time
+from bluetooth import *
+
+NORTH = 10
+EAST = 20
+SOUTH = 30
+WEST = 40
+
+WAITING = 50
+RUNNING = 60
+
+MASTER = 70
+SLAVE = 80
+
+SLAVE_COUNT = 1
+
+
+robot = {
+    "tile": None,
+    "direction": NORTH,
+    "status": WAITING,
+    "type": MASTER,
+    "uuid": "00000000-0000-0000-0000-000000000001",
+    "map": {
+        "tiles": [
+            {
+                "position": [0, 0],
+                "begin": True,
+                "end": False,
+                "input_dir": [],
+                "output_dirs": [],
+                "possible_dirs": [NORTH],
+                "forbidden_dirs": [WEST, SOUTH, EAST]
+            }
+        ]
+    }
 }
 
+server_sock=BluetoothSocket( RFCOMM )
+server_sock.bind(("",PORT_ANY))
+server_sock.listen(1)
 
-# self.position = self.map["tiles"][0]
+port = server_sock.getsockname()[1]
 
-act[0,0]
+
+advertise_service( server_sock, "RIAM_1",
+                   service_id = robot["uuid"],
+                   service_classes = [ robot["uuid"], SERIAL_PORT_CLASS ],
+                   profiles = [ SERIAL_PORT_PROFILE ],
+                  )
+
+
+# robot["tile"] = robot["map"]["tiles"][0]
 
 def act(position):
-    self.position = tile(position)
-    if self.position["end"] is True:
+    print(position)
+    robot["tile"] = tile(position)
+    if robot["tile"]["end"] is True:
         # notify_finish() TODO
         return True
     else:
-        while self.position["possible_dirs"].length > 0:
-            direction = self.position["possible_dirs"].pop()
+        while len(robot["tile"]["possible_dirs"]) > 0:
+            direction = robot["tile"]["possible_dirs"].pop()
             if check(direction):
-                self.position["output_dirs"].append(direction)
+                robot["tile"]["output_dirs"].append(direction)
                 move()
-                # notify_and_wait()
+                notify_and_wait()
                 if act(next_position(position, direction)):
                     return True
             else:
-                self.position["forbidden_dirs"].append(direction)
+                robot["tile"]["forbidden_dirs"].append(direction)
 
-        look_at(self.position["input_dir"])
+        look_at(robot["tile"]["input_dir"])
         move()
-        # notify_and_wait()
+        notify_and_wait()
         return False
 
 
 
 def next_position(position, direction):
-    if direction is NORTH
+    if direction is NORTH:
         return [position[0], position[1] + 1]
-    elif direction is EAST
+    elif direction is EAST:
         return [position[0] + 1, position[1]]
-    elif direction is SOUTH
+    elif direction is SOUTH:
         return [position[0], position[1] - 1]
-    elif direction is WEST
+    else:
         return [position[0] - 1, position[1]]
 
 
-def tile(x, y):
-    #TODO return a tile in map.
+def tile(position):
+    exists = False
+    for i in range(0, len(robot["map"]["tiles"])):
+        tile = robot["map"]["tiles"][i];
+        if tile["position"][0] is position[0] and tile["position"][1] is position[1]:
+            exists = True
+            break
+
+
+    if not exists:
+        tile = {
+            "position": position,
+            "begin": True,
+            "end": False,
+            "input_dir": [opposite(robot["direction"])],
+            "output_dirs": [],
+            "possible_dirs": directions_except(opposite(robot["direction"])),
+            "forbidden_dirs": []
+        }
+        robot["map"]["tiles"].append(tile)
+    return tile
+
+def directions_except(except_dir):
+    dirs = [NORTH, WEST, EAST, SOUTH]
+    dirs.remove(except_dir)
+    return dirs
+
+def opposite(direction):
+    if direction is NORTH:
+        return SOUTH
+    elif direction is EAST:
+        return WEST
+    elif direction is SOUTH:
+        return NORTH
+    else:
+        return EAST
+
+def notify_and_wait():
+    if robot["type"] is SLAVE:
+        notify()
+    robot["status"] = WAITING
+
+    count = 0
+    while (robot["type"] is SLAVE and count < 1) or (robot["type"] is MASTER and count < SLAVE_COUNT):
+        print("Waiting for connection on RFCOMM channel %d" % port)
+        client_sock, client_info = server_sock.accept()
+
+        try:
+            while True:
+                data = client_sock.recv(1024)
+                if len(data) == 0: break
+                print("received [%s]" % data)
+        except IOError:
+            pass
+
+        print("disconnected")
+
+        client_sock.close()
+        update_data(data)
+        count += 1
+
+
+    robot["status"] = RUNNING
+
+
+
+def update_data(data):
+    #TODO save new info
     pass
 
+def notify():
+    uuid = "00000000-0000-0000-0000-000000000001" #MASTER UUID
 
-def notify_and_wait:
-    notify()
-    self.status = WAITING
-    while self.status is WAITING:
-        pass
+    while True:
+        service_matches = find_service( uuid = uuid, address = addr )
 
-def notify:
-    #TODO query master new map info
-    pass
+        if len(service_matches) == 0:
+            print("Couldn't notify. Retrying...")
+            time.sleep(0.2)
+        else:
+
+            first_match = service_matches[0]
+            port = first_match["port"]
+            name = first_match["name"]
+            host = first_match["host"]
+
+            print("connecting to \"%s\" on %s" % (name, host))
+
+            # Create the client socket
+            sock=BluetoothSocket( RFCOMM )
+            sock.connect((host, port))
+
+            sock.send('{"foo": "bar"}')
+
+            sock.close()
+            break
 
 def move(steps=1):
     #TODO gpio move
@@ -80,41 +198,41 @@ def check(direction):
     look(direction)
     return path_clear()
 
-def path_clear:
+def path_clear():
     #TODO gpio check if front is blocked
     return True
 
-def look_north:
-    if self.direction is EAST
+def look_north():
+    if robot["direction"] is EAST:
         rotate(90)
-    elif self.direction is SOUTH
+    elif robot["direction"] is SOUTH:
         rotate(180)
-    elif self.direction is WEST
+    elif robot["direction"] is WEST:
         rotate(-90)
 
 
-def look_east:
-    if self.direction is SOUTH
+def look_east():
+    if robot["direction"] is SOUTH:
         rotate(90)
-    elif self.direction is WEST
+    elif robot["direction"] is WEST:
         rotate(180)
-    elif self.direction is NORTH
+    elif robot["direction"] is NORTH:
         rotate(-90)
 
-def look_south:
-    if self.direction is WEST
+def look_south():
+    if robot["direction"] is WEST:
         rotate(90)
-    elif self.direction is NORTH
+    elif robot["direction"] is NORTH:
         rotate(180)
-    elif self.direction is EAST
+    elif robot["direction"] is EAST:
         rotate(-90)
 
-def look_west:
-    if self.direction is SOUTH
+def look_west():
+    if robot["direction"] is SOUTH:
         rotate(90)
-    elif self.direction is EAST
+    elif robot["direction"] is EAST:
         rotate(180)
-    elif self.direction is NORTH
+    elif robot["direction"] is NORTH:
         rotate(-90)
 
 def look(direction):
@@ -124,5 +242,9 @@ def look(direction):
         look_east()
     elif direction is SOUTH:
         look_south()
-    else
+    else:
         look_west()
+
+act([0,0])
+client_sock.close()
+server_sock.close()
